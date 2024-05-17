990c2b265cb447bca16c0afc43c87cfaba3ec550
snow-man01ch <Ratthakorn.a@gmail.com>
2024-05-17 21:59:21 +0700

    v3

--- src/components/LoginRegister/LoginRegister.jsx
+++ src/components/LoginRegister/LoginRegister.jsx
@@ -1,36 +1,75 @@
 import BG from "../../assets/BG.mp4";
 import { Link } from "react-router-dom";
-export default function LoginRegister(){
-    return(
-        <div className="main w-[100%] h-[100vh]">
-            <video className=" w-[100%] h-[100%] object-cover" src={BG} autoPlay loop muted/>
-            <div className="absolute w-[100%] h-[100%] top-0 ">
-                <div>
-                    <div className=''>
-                        <form action="" className=" m-[200px] max-w-[250px] mx-auto justify-center items-center bg-cover bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-35 relative">
-                        <h1 className=" text-4xl text-white text-center mb-6 font-bold">Login</h1>
-                        <div className=" relative my-4">
-                            <input type="text" className=" block w-72 py-2.3 px-0 text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer" />
-                            <label htmlFor="">Your Username</label>
-                        </div>
-                        <div className=" relative my-4">
-                            <input type="password" className=" block w-72 py-2.3 px-0 text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer" />
-                            <label htmlFor="">Your Password</label>
-                        </div>
-                        <div>
-                            <div>
-                                <input type="checkbox" name="" id=""/>
-                                <label htmlFor="Remeber Me"></label>
-                            </div>
-                            <span>Forgot Password?</span>
-                        </div>
-                        <button type="submit">Login</button>
-                        <span>New Here?<Link to="Register">Create an Account</Link></span>
-                        </form>
-                    </div>
-                </div>
+import imglogin from "../../assets/imglogin1.jpg";
+
+export default function LoginRegister() {
+  return (
+    <div className="relativ w-full bg-zinc-900/40 text-white h-[100vh] flex justify-center items-center ">
+      <img
+        src={imglogin}
+        alt=""
+        className="absolute w-full h-full object-cover mix-blend-color-burn"
+      />
+      <div className="">
+        <div className="bg-gray-900 border border-gray-400 rounded-md p-20 shadow-lg  backdrop-blur-lg bg-opacity-20 relative">
+          <h1 className="text-4xl text-white font-bold text-center mb-10">
+            Member Login
+          </h1>
+          <form action="">
+            <div className=" pb-2 relative my-6">
+              <input
+                type="email"
+                className="block w-72 py-2 px-1 text-md font-semibold text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-gray-800 focus:outline-none focus:ring-0 focus:text-white focus:border-gray-800 peer "
+                placeholder=""
+              />
+              <label
+                htmlFor=""
+                className="absolute text-xm font-semibold text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-800 peer-focus:dark:text-gray-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7"
+              >
+                Email :
+              </label>
             </div>
-            
+            <div className="relative my-6">
+              <input
+                type="password"
+                className="block w-72 py-2 text-md px-1 font-semibold text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-gray-800 focus:outline-none focus:ring-0 focus:text-white focus:border-gray-800 peer"
+                placeholder=""
+              />
+              <label
+                htmlFor=""
+                className="absolute text-xm font-semibold  text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-800 peer-focus:dark:text-gray-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7"
+              >
+                Password :
+              </label>
+            </div>
+            <div>
+              <div>
+                <input type="checkbox" />
+                <label htmlFor="Remember Me" className="ml-2">
+                  Remember Me
+                </label>
+              </div>
+            </div>
+            <button
+              className="w-full mb-4 text-[18px] font-bold mt-6 rounded-full bg-white text-emerald-800 hover:text-white hover:bg-emerald-400 py-2  duration-200  transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
+              type="submit"
+            >
+              Sign In
+            </button>
+            <div>
+              <span>
+                Not a member?{" "}
+                <Link
+                  to="Register"
+                  className="font-bold text-emerald-300 hover:text-emerald-400 duration-300 hover:text-sm"
+                >
+                  Create an Account{" "}
+                </Link>
+              </span>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
