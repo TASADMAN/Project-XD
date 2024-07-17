66bfb7a11cc551f296479bbe4bfa34438b214531
snow-man01ch <Ratthakorn.a@gmail.com>
2024-07-17 15:54:26 +0700

    fix member

--- src/components/LoginRegister/RegisterMember.jsx
+++ src/components/LoginRegister/RegisterMember.jsx
@@ -20,19 +20,7 @@ const RegisterMember = () => {
             Member Register
           </h1>
           <form action="">
-            <div className=" pb-2 relative my-6">
-              <input
-                type="text"
-                className="block w-72 py-2 px-1 text-md font-semibold text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-gray-800 focus:outline-none focus:ring-0 focus:text-white focus:border-gray-800 peer "
-                placeholder=""
-              />
-              <label
-                htmlFor=""
-                className="absolute text-xm font-semibold text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-800 peer-focus:dark:text-gray-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7"
-              >
-                Name :
-              </label>
-            </div>
+            
             <div className=" pb-2 relative my-6">
               <input
                 type="email"
@@ -57,6 +45,19 @@ const RegisterMember = () => {
                 className="absolute text-xm font-semibold text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-800 peer-focus:dark:text-gray-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7"
               >
                 Phone Number :
+              </label>
+            </div>
+            <div className=" pb-2 relative my-6">
+              <input
+                type="text"
+                className="block w-72 py-2 px-1 text-md font-semibold text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-gray-800 focus:outline-none focus:ring-0 focus:text-white focus:border-gray-800 peer "
+                placeholder=""
+              />
+              <label
+                htmlFor=""
+                className="absolute text-xm font-semibold text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-800 peer-focus:dark:text-gray-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7"
+              >
+                User Name :
               </label>
             </div>
             <div className="relative my-6">
