77fe5095cdb16c00f6ef8ce44fc6dd205a4e7009
TASADMAN <0829037403zz@gmail.com>
2024-07-29 00:06:15 +0700

    add text-white

--- src/components/Booking/Calendar.jsx
+++ src/components/Booking/Calendar.jsx
@@ -199,7 +199,7 @@ export default function Calendar() {
                           <button
                             type="button"
                             data-collapse-toggle="timetable"
-                            className="inline-flex items-center w-full py-2 px-5 me-2 justify-center text-sm font-medium  focus:outline-none  rounded-lg border border-gray-200 bg-gray-800 text-gray-400 dark:border-gray-600 dark:hover:text-whitehover:bg-gray-700"
+                            className=" inline-flex items-center w-full py-2 px-5 me-2 justify-center text-sm font-medium  focus:outline-none  rounded-lg border border-gray-200 bg-gray-800 text-white dark:border-gray-600 dark:hover:text-whitehover:bg-gray-700"
                           >
                             <svg
                               className="h-6 w-6"
@@ -210,7 +210,7 @@ export default function Calendar() {
                             >
                               <path d="M112 48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm32 128.1L144 304l32 0 0-127.9c-.7 0-1.5-.1-2.3-.1l-27.5 0c-.8 0-1.5 0-2.3 .1zM144 352l0 136c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-264.4L52.9 299.8c-6.5 11.5-21.2 15.6-32.7 9.1s-15.6-21.2-9.1-32.7L69.7 172.7c15.6-27.6 44.9-44.7 76.6-44.7l27.5 0c31.7 0 61 17.1 76.6 44.7l58.5 103.5c6.5 11.5 2.5 26.2-9.1 32.7s-26.2 2.5-32.7-9.1L224 223.6 224 488c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-136-32 0z" />
                             </svg>
-                            <span className="me-3 ms-2">15</span>
+                            <span className="me-3 ms-2 text-white">15</span>
                              คน
                           </button>
                           <label className="sr-only">Pick a time</label>
